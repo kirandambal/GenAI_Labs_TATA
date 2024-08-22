3cdbb48f0dd421217589d1443b10b10d381d44c4
Kiran Dambal <61645227+kirandambal@users.noreply.github.com>
2024-08-22 15:31:35 +0530

    Create sample.py

---
+++ Speech Assistant/sample.py
@@ -0,0 +1,40 @@
+#Speech to Text
+def speechrec():
+    command = ''
+
+     # Configure speech service
+    speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
+    print('Ready to use speech service in:', speech_config.region)
+    # Configure speech recognition
+    # Configure speech recognition
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+
+
+    while command.lower() != 'quit.':
+        print('User :')    
+         # Process speech input
+        speech = speech_recognizer.recognize_once_async().get()
+        if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+            command = speech.text
+            print(command)
+        else:
+            print(speech.reason)
+            if speech.reason == speech_sdk.ResultReason.Canceled:
+                cancellation = speech.cancellation_details
+                print(cancellation.reason)
+                print(cancellation.error_details)   
+        azopenai(command)
+
+#Text to Speech
+def speechsys(response_text):
+
+    # Configure speech synthesis
+    # Configure speech synthesis
+    speech_config.speech_synthesis_voice_name = 'en-GB-LibbyNeural' # change this
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+
+    # Synthesize spoken output
+    speak = speech_synthesizer.speak_text_async(response_text).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
